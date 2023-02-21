c68801869a4bd080aa60671b1859bb9a2e189b54
fulgurance <zohran.londais@gmail.com>
2023-02-21 13:50:23 +0000

    Patched Bash to support Pass option

--- Bash/5.1.8/5.1.8.cr
+++ Bash/5.1.8/5.1.8.cr
@@ -2,11 +2,19 @@ class Target < ISM::Software
     
     def configure
         super
-        configureSource([   "--prefix=/usr",
-                            "--docdir=/usr/share/doc/bash-5.1.8",
-                            "--without-bash-malloc",
-                            "--with-installed-readline"],
-                            buildDirectoryPath)
+        if option("Pass1")
+            configureSource([   "--prefix=#{Ism.settings.rootPath}usr",
+                                "--build=$(support/config.guess)",
+                                "--host=#{Ism.settings.target}",
+                                "--without-bash-malloc"],
+                                buildDirectoryPath)
+        else
+            configureSource([   "--prefix=/usr",
+                                "--docdir=/usr/share/doc/bash-5.1.8",
+                                "--without-bash-malloc",
+                                "--with-installed-readline"],
+                                buildDirectoryPath)
+        end
     end
     
     def build
@@ -16,7 +24,12 @@ class Target < ISM::Software
     
     def prepareInstallation
         super
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        if option("Pass1")
+            makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}","install"],buildDirectoryPath)
+            makeLink("bash","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}bin/sh",:symbolicLink)
+        else
+            makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        end
     end
 
 end
