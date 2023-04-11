2ef7ce527133ac2c9b659b2c04d9a631d7c7b715
Fulgurance <zohran.londais@gmail.com>
2023-04-11 10:23:16 +0100

    Patched prepareInstallation phase

--- Bash/5.1.8/5.1.8.cr
+++ Bash/5.1.8/5.1.8.cr
@@ -29,90 +29,94 @@ class Target < ISM::Software
 
         makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
 
-        profileData = <<-CODE
-        pathremove () {
-        local IFS=':'
-        local NEWPATH
-        local DIR
-        local PATHVARIABLE=${2:-PATH}
-        for DIR in ${!PATHVARIABLE} ; do
-                if [ "$DIR" != "$1" ] ; then
-                  NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
-                fi
-        done
-        export $PATHVARIABLE="$NEWPATH"
-        }
+        if !option("Pass1")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc")
 
-        pathprepend () {
-                pathremove $1 $2
-                local PATHVARIABLE=${2:-PATH}
-                export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
-        }
+            profileData = <<-CODE
+            pathremove () {
+            local IFS=':'
+            local NEWPATH
+            local DIR
+            local PATHVARIABLE=${2:-PATH}
+            for DIR in ${!PATHVARIABLE} ; do
+                    if [ "$DIR" != "$1" ] ; then
+                    NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
+                    fi
+            done
+            export $PATHVARIABLE="$NEWPATH"
+            }
 
-        pathappend () {
-                pathremove $1 $2
-                local PATHVARIABLE=${2:-PATH}
-                export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
-        }
+            pathprepend () {
+                    pathremove $1 $2
+                    local PATHVARIABLE=${2:-PATH}
+                    export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
+            }
 
-        export -f pathremove pathprepend pathappend
+            pathappend () {
+                    pathremove $1 $2
+                    local PATHVARIABLE=${2:-PATH}
+                    export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
+            }
 
-        export PATH=/usr/bin
+            export -f pathremove pathprepend pathappend
 
-        if [ ! -L /bin ]; then
-                pathappend /bin
-        fi
+            export PATH=/usr/bin
 
-        if [ $EUID -eq 0 ] ; then
-                pathappend /usr/sbin
-                if [ ! -L /sbin ]; then
-                        pathappend /sbin
-                fi
-                unset HISTFILE
-        fi
+            if [ ! -L /bin ]; then
+                    pathappend /bin
+            fi
 
-        export HISTSIZE=1000
-        export HISTIGNORE="&:[bf]g:exit"
+            if [ $EUID -eq 0 ] ; then
+                    pathappend /usr/sbin
+                    if [ ! -L /sbin ]; then
+                            pathappend /sbin
+                    fi
+                    unset HISTFILE
+            fi
 
-        #export XDG_DATA_DIRS=${XDG_DATA_DIRS:-/usr/share/}
-        #export XDG_CONFIG_DIRS=${XDG_CONFIG_DIRS:-/etc/xdg/}
-        #export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/tmp/xdg-$USER}
+            export HISTSIZE=1000
+            export HISTIGNORE="&:[bf]g:exit"
 
-        NORMAL="\\[\\e[0m\\]"
-        RED="\\[\\e[1;31m\\]"
-        GREEN="\\[\\e[1;32m\\]"
-        if [[ $EUID == 0 ]] ; then
-        PS1="$RED\\u [ $NORMAL\\w$RED ]# $NORMAL"
-        else
-        PS1="$GREEN\\u [ $NORMAL\\w$GREEN ]\\$ $NORMAL"
-        fi
+            #export XDG_DATA_DIRS=${XDG_DATA_DIRS:-/usr/share/}
+            #export XDG_CONFIG_DIRS=${XDG_CONFIG_DIRS:-/etc/xdg/}
+            #export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/tmp/xdg-$USER}
 
-        for script in /etc/profile.d/*.sh ; do
-                if [ -r $script ] ; then
-                        . $script
-                fi
-        done
+            NORMAL="\\[\\e[0m\\]"
+            RED="\\[\\e[1;31m\\]"
+            GREEN="\\[\\e[1;32m\\]"
+            if [[ $EUID == 0 ]] ; then
+            PS1="$RED\\u [ $NORMAL\\w$RED ]# $NORMAL"
+            else
+            PS1="$GREEN\\u [ $NORMAL\\w$GREEN ]\\$ $NORMAL"
+            fi
 
-        unset script RED GREEN NORMAL
-        CODE
-        fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile",profileData)
+            for script in /etc/profile.d/*.sh ; do
+                    if [ -r $script ] ; then
+                            . $script
+                    fi
+            done
 
-        bashrcData = <<-CODE
-        alias ls='ls --color=auto'
-        alias grep='grep --color=auto'
+            unset script RED GREEN NORMAL
+            CODE
+            fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile",profileData)
 
-        NORMAL="\\[\\e[0m\\]"
-        RED="\\[\\e[1;31m\\]"
-        GREEN="\\[\\e[1;32m\\]"
-        if [[ $EUID == 0 ]] ; then
-        PS1="$RED\\u [ $NORMAL\\w$RED ]# $NORMAL"
-        else
-        PS1="$GREEN\\u [ $NORMAL\\w$GREEN ]\\$ $NORMAL"
-        fi
+            bashrcData = <<-CODE
+            alias ls='ls --color=auto'
+            alias grep='grep --color=auto'
 
-        unset RED GREEN NORMAL
-        CODE
-        fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/bashrc",bashrcData)
+            NORMAL="\\[\\e[0m\\]"
+            RED="\\[\\e[1;31m\\]"
+            GREEN="\\[\\e[1;32m\\]"
+            if [[ $EUID == 0 ]] ; then
+            PS1="$RED\\u [ $NORMAL\\w$RED ]# $NORMAL"
+            else
+            PS1="$GREEN\\u [ $NORMAL\\w$GREEN ]\\$ $NORMAL"
+            fi
+
+            unset RED GREEN NORMAL
+            CODE
+            fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/bashrc",bashrcData)
+        end
     end
 
     def install
