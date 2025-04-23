5266ce902e8dc13cb07bd0d065608c0bae1dc612
Fulgurance <zohran.londais@gmail.com>
2025-04-23 14:19:38 +0100

    Patched build value

--- Bash/5.2.37/5.2.37.cr
+++ Bash/5.2.37/5.2.37.cr
@@ -5,7 +5,7 @@ class Target < ISM::Software
 
         if option("Pass1")
             configureSource(arguments:  "--prefix=/usr                          \
-                                        --build=$(support/config.guess)         \
+                                        --build=#{Ism.settings.chrootTarget}    \
                                         --host=#{Ism.settings.chrootTarget}     \
                                         bash_cv_strtold_broken=no               \
                                         --without-bash-malloc",
