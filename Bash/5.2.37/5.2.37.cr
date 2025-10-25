b8f149121f8b525c1112702c4c4457d7b02e25b6
Fulgurance <zohran.londais@gmail.com>
2025-10-25 15:39:26 +0100

    Updated to last ISM standard

--- Bash/5.2.37/5.2.37.cr
+++ Bash/5.2.37/5.2.37.cr
@@ -6,7 +6,7 @@ class Target < ISM::Software
         if option("Pass1")
             configureSource(arguments:  "--prefix=/usr                                                  \
                                         --build=$(sh support/config.guess)                              \
-                                        --host=#{Ism.settings.chrootTarget}                             \
+                                        --host=#{Ism.settings.chrootSystemTarget}                             \
                                         bash_cv_strtold_broken=no                                       \
                                         --without-bash-malloc",
                         path:           buildDirectoryPath)
